a867a1d0dbe9b76543a1d9e53bf384528010b807
Alif Samsuddin <alifsams@umich.edu>
2017-03-18 21:04:50 -0400

    Add .travis.yml and fixed rpn files

--- rpn.py
+++ rpn.py
@@ -3,26 +3,33 @@
 import operator
 
 operators = {
-	"+": operator.add,
-	"-": operator.sub,
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
+	'^': operator.pow,
 }
 
 def calculate(arg):
 	stack = list()
 	for operand in arg.split():
 		try:
-			operand = float(operand)
+			operand = int(operand)
 			stack.append(operand)
 		except ValueError:
 			arg2 = stack.pop()
 			arg1 = stack.pop()
-			stack.append.operators(operand)(arg1,arg2)
+			result = function(arg1, arg2)
+			stack.append(result)
+		print(stack)
+	if len(stack) != 1:
+		raise TypeError("Too many parameters")
 	return stack.pop()
 
 def main():
 	while True:
 		result = calculate(input("rpn calc> "))
-		print("Result = ", result)
+		print("Result: ", result)
 
 if __name__ == '__main__':
 	main()
